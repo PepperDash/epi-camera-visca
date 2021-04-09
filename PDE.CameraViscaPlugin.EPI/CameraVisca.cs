bd4905af4eda2c662a3bc505b3e99531eb740386
Maxim Batourine <m.batourine@gmail.com>
2021-04-08 22:58:19 -0400

    Cleanups
    Add Poll functionality
    Enabled property
    PTZ Speed Increase Behavior
    Cleanup BridgeJoinMap class

--- PDE.CameraViscaPlugin.EPI/CameraVisca.cs
+++ PDE.CameraViscaPlugin.EPI/CameraVisca.cs
@@ -21,6 +21,24 @@ namespace PDE.CameraViscaPlugin.EPI
 		private readonly GenericCommunicationMonitor _commsMonitor;
         private readonly ViscaProtocolProcessor _visca;
         private readonly ViscaCamera _camera;
+        /// <summary>
+        /// Holds all defined Camera Poll commands and it's string key
+        /// </summary>
+        private readonly Dictionary<string, Action> _cameraPollCommands;
+        /// <summary>
+        /// Holds configured to poll commands
+        /// </summary>
+        private readonly List<Action> _poll = new List<Action>();
+
+        /// <summary>
+        /// Used to determine when to move the camera at a faster speed if a direction is held
+        /// </summary>
+        private readonly bool _ptzSpeedIncreaseBehaivor;
+        private CTimerCallbackFunction _ptzSpeedIncreaseAction;
+        private CTimer _ptzSpeedIncreaseTimer;
+        private byte _ptzPanNormalSpeed;
+        private byte _ptzTiltNormalSpeed;
+
         public ViscaCamera Camera { get { return _camera; } }
 
         /// <summary>
@@ -59,35 +77,101 @@ namespace PDE.CameraViscaPlugin.EPI
             _camera.PowerChanged += (o, e) => { PowerIsOnFeedback.FireUpdate(); CameraIsOffFeedback.FireUpdate(); };
             _camera.MuteChanged += (o, e) => { CameraIsMutedFeedback.FireUpdate(); };
 
+            _cameraPollCommands = new Dictionary<string, Action>()
+            {
+                {"AE",            _camera.AEPoll},
+                {"Aperture",      _camera.AperturePoll},
+                {"BackLight",     _camera.BackLightPoll},
+                {"BGain",         _camera.BGainPoll},
+                {"ExpComp",       _camera.ExpCompPoll},
+                {"FocusAuto",     _camera.FocusAutoPoll},
+                {"FocusPosition", _camera.FocusPositionPoll},
+                {"Gain",          _camera.GainPoll},
+                {"Iris",          _camera.IrisPoll},
+                {"Mute",          _camera.MutePoll},
+                {"PTZPosition",   _camera.PanTiltPositionPoll},
+                {"Power",         _camera.PowerPoll},
+                {"RGain",         _camera.RGainPoll},
+                {"Shutter",       _camera.ShutterPoll},
+                {"Title",         _camera.TitlePoll},
+                {"WB",            _camera.WBModePoll},
+                {"WD",            _camera.WideDynamicModePoll},
+                {"ZoomPosition",  _camera.ZoomPositionPoll},
+            };
+
             _comms = comm;
+
+            // TODO: For VISCA camera current library implementation only
+            // serial is supported, so socket code is not needed
 			var socket = _comms as ISocketStatus;
 			if (socket != null)
 			{
-				// device comms is IP **ELSE** device comms is RS232
 				socket.ConnectionChange += socket_ConnectionChange;
-				Connect = true;
+                if (Enabled)
+				    Connect = true;
             }
 
             if (_config.CommunicationMonitorProperties != null)
             {
+                if (!String.IsNullOrEmpty(_config.CommunicationMonitorProperties.PollString))
+                    foreach (var poll in _config.CommunicationMonitorProperties.PollString.Split(','))
+                        if (_cameraPollCommands.ContainsKey(poll.Trim()))
+                            _poll.Add(_cameraPollCommands[poll.Trim()]);
+
                 _commsMonitor = new GenericCommunicationMonitor(this, _comms,
                     _config.CommunicationMonitorProperties.PollInterval,
                     _config.CommunicationMonitorProperties.TimeToWarning,
                     _config.CommunicationMonitorProperties.TimeToError,
-                    _camera.Poll);
+                    () => _poll.ForEach((pollAction) => pollAction()) 
+                    );
             }
             else
             {
-                _commsMonitor = new GenericCommunicationMonitor(this, _comms, 10000, 20000, 30000, _camera.Poll);
+                // We do not have CommunicationMonitorProperties and therefore
+                // no Poll string defined, using ALL poll functions
+                _poll.AddRange(_cameraPollCommands.Values);
+                _commsMonitor = new GenericCommunicationMonitor(this, _comms, 10000, 20000, 30000, () => _poll.ForEach((pollAction) => pollAction()));
             }
 
-            _commsMonitor.Client.BytesReceived += (s, e) => { if (Enabled) _visca.ProcessIncomingData(e.Bytes); };
+            _commsMonitor.Client.BytesReceived += (s, e) => _visca.ProcessIncomingData(e.Bytes);
+            DeviceManager.AddDevice(CommunicationMonitor);
+
+            // Handle Increase PTZ move speed
+            #region PTZ Speed Increase
+
+            // if FastSpeedHoldTimeMs defined in config, enable SpeedIncrease behaivor
+            if (_config.FastSpeedHoldTimeMs > 0)
+            {
+                _ptzSpeedIncreaseBehaivor = true;
+
+                if (_config.PanSpeedSlow > 0)
+                    _camera.PanSpeed = _config.PanSpeedSlow;
+
+                if (_config.TiltSpeedSlow > 0)
+                    _camera.TiltSpeed = _config.TiltSpeedSlow;
+
+                _ptzPanNormalSpeed = _camera.PanSpeed;
+                _ptzTiltNormalSpeed = _camera.TiltSpeed;
+
+                _ptzSpeedIncreaseAction = ptzCommand =>
+                {
+                    // kill timer
+                    ptzSpeedChangeTimerDispose();
+                    // if Fast speeds not defined, use Max values
+                    _camera.PanSpeed = (_config.PanSpeedFast > 0) ? _config.PanSpeedFast : Visca.ViscaDefaults.PanSpeedLimits.High;
+                    _camera.TiltSpeed = (_config.TiltSpeedFast > 0) ? _config.PanSpeedFast : Visca.ViscaDefaults.TiltSpeedLimits.High;
+                    // we passed current ptz command to action, so we repeat it with increased speed 
+                    (ptzCommand as Action).Invoke();
+                };
+            }
+
+            #endregion PTZ Speed Increase
         }
 
         public override bool CustomActivate()
         {
             _commsMonitor.StatusChange += (o, a) => { Debug.Console(2, this, "Communication monitor state: {0}", _commsMonitor.Status); };
-            Connect = true;
+            if (Enabled) Connect = true;
             return true;
         }
 
@@ -158,52 +242,7 @@ namespace PDE.CameraViscaPlugin.EPI
         /// <param name="bridge"></param>
         public void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge)
         {
-            var joinMap = new CameraViscaBridgeJoinMap(joinStart);
-
-            // This adds the join map to the collection on the bridge
-            if (bridge != null)
-            {
-                bridge.AddJoinMap(Key, joinMap);
-            }
-
-            var customJoins = JoinMapHelper.TryGetJoinMapAdvancedForDevice(joinMapKey);
-
-            if (customJoins != null)
-            {
-                joinMap.SetCustomJoinData(customJoins);
-            }
-
-            Debug.Console(1, "Linking to Trilist '{0}'", trilist.ID.ToString("X"));
-            Debug.Console(0, "Linking to Bridge Type {0}", GetType().Name);
-
-            // TODO [ ] Implement bridge links as needed
-
-            // links to bridge
-            trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
-
-            trilist.SetBoolSigAction(joinMap.Connect.JoinNumber, sig => Connect = sig);
-            ConnectFeedback.LinkInputSig(trilist.BooleanInput[joinMap.Connect.JoinNumber]);
-
-            StatusFeedback.LinkInputSig(trilist.UShortInput[joinMap.Status.JoinNumber]);
-            OnlineFeedback.LinkInputSig(trilist.BooleanInput[joinMap.IsOnline.JoinNumber]);
-
-            UpdateFeedbacks();
-
-            trilist.OnlineStatusChange += (o, a) =>
-            {
-                if (!a.DeviceOnLine) return;
-
-                trilist.SetString(joinMap.DeviceName.JoinNumber, Name);
-                UpdateFeedbacks();
-            };
-        }
-
-        private void UpdateFeedbacks()
-        {
-            // TODO [ ] Update as needed for the plugin being developed
-            ConnectFeedback.FireUpdate();
-            OnlineFeedback.FireUpdate();
-            StatusFeedback.FireUpdate();
+            LinkCameraToApi(this, trilist, joinStart, joinMapKey, bridge);
         }
 
         #endregion
@@ -253,21 +292,21 @@ namespace PDE.CameraViscaPlugin.EPI
 
         #region IHasCameraPanControl Members
 
-        public void PanLeft()  { _camera.Left(); }
+        public void PanLeft() { ptzCommand((Action)_camera.Left); }
 
-        public void PanRight()  { _camera.Right(); }
+        public void PanRight() { ptzCommand((Action)_camera.Right); }
 
-        public void PanStop()  { _camera.Stop(); }
+        public void PanStop() { ptzStop(); }
 
         #endregion
 
         #region IHasCameraTiltControl Members
 
-        public void TiltDown() { _camera.Down(); }
+        public void TiltDown() { ptzCommand((Action)_camera.Down); }
 
-        public void TiltStop()  { _camera.Stop(); }
+        public void TiltUp() { ptzCommand((Action)_camera.Up); }
 
-        public void TiltUp() { _camera.Up(); }
+        public void TiltStop() { ptzStop(); }
 
         #endregion
 
@@ -280,6 +319,47 @@ namespace PDE.CameraViscaPlugin.EPI
         public void ZoomStop() { _camera.ZoomStop(); }
 
         #endregion IHasCameraZoomControl Members
+
+        #region PTZ SpeedIncrease Behaivor Implementation
+
+        private void ptzCommand(Action ptzCommand)
+        {
+            if (_ptzSpeedIncreaseBehaivor)
+            {
+                ptzSpeedChangeTimerDispose();
+                ptzSpeedReset();
+                _ptzSpeedIncreaseTimer = new CTimer(_ptzSpeedIncreaseAction, ptzCommand, _config.FastSpeedHoldTimeMs);
+            }
+            ptzCommand.Invoke();
+        }
+
+        private void ptzStop()
+        {
+            if (_ptzSpeedIncreaseBehaivor)
+            {
+                ptzSpeedChangeTimerDispose();
+                ptzSpeedReset();
+            }
+            _camera.Stop();
+        }
+
+        private void ptzSpeedChangeTimerDispose()
+        {
+            if (_ptzSpeedIncreaseTimer != null)
+            {
+                _ptzSpeedIncreaseTimer.Stop();
+                _ptzSpeedIncreaseTimer.Dispose();
+                _ptzSpeedIncreaseTimer = null;
+            }
+        }
+
+        private void ptzSpeedReset()
+        {
+            _camera.PanSpeed = _ptzPanNormalSpeed;
+            _camera.TiltSpeed = _ptzTiltNormalSpeed;
+        }
+
+        #endregion PTZ SpeedIncrease Behaivor Implementation
 
         #endregion IHasCameraPtzControl Members
 
@@ -319,12 +399,9 @@ namespace PDE.CameraViscaPlugin.EPI
 
         #region IHasCameraPresets Members
 
-        public void PresetSelect(int preset) { _camera.MemoryRecall((byte) preset);}
+        public void PresetSelect(int preset) { _camera.MemoryRecall((byte) preset); }
 
-        public void PresetStore(int preset, string description)
-        {
-            throw new NotImplementedException();
-        }
+        public void PresetStore(int preset, string description) { _camera.MemorySet((byte) preset); }
 
         public List<CameraPreset> Presets { get { return _config.Presets; } }
 
